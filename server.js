d1639acbc2f30b7c14d9d998ab080d9e685e3b69
Josue19lp <josueanti@gmail.com>
2025-09-18 22:45:01 -0600

    Mensaje del commit

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use((req, res, next) => {
 // Configuración de MySQL
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
